c01c14cddabbbd0471ad513f19741a893546eea5
Anderson Coelho <anderson-tomas@hotmail.com>
2018-11-28 23:00:35 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,25 +1,31 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
+def display_card_total(total)
   # code #display_card_total here
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
   # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
   # code #get_user_input here
+  return gets.chomp
 end
 
 def end_game
   # code #end_game here
+  puts "Sorry, you hit 27. Thanks for playing!"
 end
 
 def initial_round
@@ -41,4 +47,3 @@ end
 def runner
   # code runner here
 end
-    
